8160024f1c4863dfc97239c79dda28df83165b96
janatlk <janat00991@gmail.com>
2022-06-19 16:11:14 +0600

    homework3

--- main/models.py
+++ main/models.py
@@ -22,10 +22,14 @@ class Director(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(default='None',blank=True)
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return self.title
 
 class Review(models.Model):
     text = models.TextField()
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE,null=True)
 
+    def __str__(self):
+        return self.text
--- main/views.py
+++ main/views.py
@@ -24,26 +24,37 @@ def today(request):
 #     product = Product.objects.get(id=id)
 #     return render(request,'main/detail.html', context={'product_detail' : product})
 
-def director_list(request):
-    director = Director.objects.all()
-    return render(request,'main/directors.html',context={'director_list':director})
-
-def director_details(request,id):
-    director = Director.objects.get(id=id)
-    return render(request,'main/directordetail.html',context={'director_detail':director})
+# def director_list(request):
+#     director = Director.objects.all()
+#     return render(request,'main/directors.html',context={'director_list':director})
+#
+# def director_details(request,id):
+#     director = Director.objects.get(id=id)
+#     return render(request,'main/directordetail.html',context={'director_detail':director})
 
 def movies_list(request):
     movie = Movie.objects.all()
     return render(request,'main/movies.html',context={'movies_list': movie})
 
 def movies_details(request,id):
-    movie = Movie.objects.get(id=id)
-    return render(request,'main/moviedetail.html',context={'movie_detail':movie})
+    context = {
+    'movie_detail' : Movie.objects.get(id=id),
+    'review' : Review.objects.filter(movie_id=id)
+    }
+    # reviews = Review.objects.filter()
+    return render(request,'main/moviedetail.html',context=context)
 
-def reviews_list(request):
-    reviews = Review.objects.all()
-    return render(request,'main/reviews.html',context={'reviews_list':reviews})
+# def movie_filter_view(request,movieid):
+#     context = {
+#         'reviews':Review.objects.filter(movie_id=movieid)
+#     }
+#     return render(request,'main/moviedetail.html',context=context)
 
-def reviews_details(request,id):
-    reviews = Review.objects.get(id=id)
-    return render(request,'main/reviewdetail.html',context={'reviews_details': reviews})
+# def reviews_list(request):
+#     # reviews = Review.objects.all()
+#     reviews = Review.objects.order_by('-id')
+#     return render(request,'main/reviews.html',context={'reviews_list':reviews})
+#
+# def reviews_details(request,id):
+#     reviews = Review.objects.get(id=id)
+#     return render(request,'main/reviewdetail.html',context={'reviews_details': reviews})
